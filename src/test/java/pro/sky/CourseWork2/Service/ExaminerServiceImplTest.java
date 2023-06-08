cef44c8dff4eb836fc3680be01b03e4903e6b5d5
Denis_vrn <denisfedorov50@gmail.com>
2023-06-08 12:06:59 +0300

    easy level

--- src/test/java/pro/sky/CourseWork2/Service/ExaminerServiceImplTest.java
+++ src/test/java/pro/sky/CourseWork2/Service/ExaminerServiceImplTest.java
@@ -1,12 +1,10 @@
 package pro.sky.CourseWork2.Service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pro.sky.CourseWork2.Model.Question;
 
@@ -21,15 +19,12 @@ class ExaminerServiceImplTest {
     private JavaQuestionService javaQuestionService;
     @InjectMocks
     private ExaminerServiceImpl examinerService;
-    @BeforeEach
-    public void setUp () {
-        javaQuestionService = new JavaQuestionService(setQuestion);
-    }
 
     @Test
     void shouldGetQuestion() {
+        Mockito.when(javaQuestionService.getAll()).thenReturn(setQuestion);
         Mockito.when(javaQuestionService.getRandomQuestion()).thenReturn(question2);
-        Collection<Question> expected = examinerService.getQuestion(2);
+        Collection<Question> expected = examinerService.getQuestion(1);
         System.out.println(expected);
         assertEquals(expected,setQuestion1);
 
